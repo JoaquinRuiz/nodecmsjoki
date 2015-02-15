6f4b3dd300acce079173a382bdbe43ba0071982d
Joaquín Ruiz <jokioki@gmail.com>
2015-02-15 21:46:06 +0000

    [feature] using events [feature] using "supervisor" [npm install -g supervisor]

--- app.js
+++ app.js
@@ -1,25 +1,68 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
 
+// Templates
+var htmlBuffer = fs.readFileSync('templates/post/new.html')
+
+// main functions
 function renderResponse(request, response){
-    var htmlBuffer = fs.readFileSync('templates/post/new.html')
+    response.writeHead(200,{
+        'Content-type': 'text/html; charset=utf-8'
+    });
 
-    response.writeHead(200,{'Content-type': 'text/html; charset=utf-8'})
-    response.end(htmlBuffer)
+    response.end(htmlBuffer);
 }
+
+function addNewPost(request, response){
+    parseBody(request, function(body){
+        var post = {
+            title : body.title,
+            content : body.content
+        }
+        console.log(post.title);
+        console.log(post.content);
+    });
+    response.end();
+}
+
+
+// Utils
 function render404(request,response){
     response.writeHead(404)
     response.end('404 File not found')
 }
 
+function parseBody(request, callback){
+    var body = '';
+    request.on('data', function(chunk){
+        body += chunk;
+    });
+
+    request.on('end', function(){
+        callback(qs.parse(body));
+    });
+
+}
+
+// Routes
+var newPostRegex = new RegExp('^/post/new/?$')
+var newRegex = new RegExp('^/post/?$')
+
+// Server
 var server = http.createServer(function callback(request,response){
-    var newPostRegex = new RegExp('^/post/new/?$')
-    var path = url.parse(request.url).pathname
-    if (newPostRegex.test(path))
+    var path = url.parse(request.url).pathname;
+
+    if (newPostRegex.test(path)){
         renderResponse(request,response)
-    else
+    }
+    else if (newRegex.test(path)){
+        addNewPost(request, response);
+    }
+    else{
         render404(request,response)
+    }
 })
 
 server.listen(8000)
